edce3866d5073db741b1f7e1e00c2f505d169369
tracer-ics2017 <tracer@njuics.org>
2024-03-09 14:50:54 +0800

    >  compile 2113665 prayer Linux prayer-virtual-machine 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 athlon i686 GNU/Linux  14:50:54 up 19 min,  1 user,  load average: 0.66, 0.40, 0.28 342ae32f5544f9736e5023aec6efbff659e40bcc

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -265,13 +265,13 @@ static uint32_t eval(int p,int q){
     /* The expression is surrounded by a matched pair of parentheses.
      * If that is the case, just throw away the parentheses.
      */
-    printf("p %d  q %d",p,q);
+    printf("p %d  q %d\n",p,q);
     return eval(p + 1, q - 1);
   }
   else {
     //op = the position of dominant operator in the token expression;
     int op=dominant_OP(p,q);
-    //printf("op:%d\n",op);
+    printf("op:%d\n",op);
     uint32_t val2 = eval(op + 1, q);
     //单目表达式
     if(tokens[op].type==TK_NEGATIVE){
