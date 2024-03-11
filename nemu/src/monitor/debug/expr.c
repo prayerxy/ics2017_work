edc295e77dde3fac6d5753d81d88182e487aa07f
tracer-ics2017 <tracer@njuics.org>
2024-03-11 19:44:56 +0800

    >  compile 2113665 prayer Linux prayer-virtual-machine 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 athlon i686 GNU/Linux  19:44:56 up  7:30,  1 user,  load average: 0.07, 0.10, 0.03 8ce1f3ab3946de31aa4a3468d1ebb46cd8efbf8

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -37,7 +37,7 @@ static struct rule {
    {"0|[1-9][0-9]*",TK_DEX},
   {"\\$(eax|ebx|ecx|edx|esp|ebp|esi|edi|eip|ax|bx|cx|dx|sp|bp|si|di|al|bl|cl|dl|ah|bh|ch|dh)",TK_REG},//all possible regs
   {"\\(", '('},       
-  {"\\)", ')'},         
+  {"\\)", ')'}        
 
 };
 
