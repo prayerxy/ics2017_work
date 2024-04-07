208a39d9d76530504fc3123c0b4e800b47490781
tracer-ics2017 <tracer@njuics.org>
2024-04-07 20:58:26 +0800

    >  compile 2113665 prayer Linux prayer-virtual-machine 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 athlon i686 GNU/Linux  20:58:26 up 1 day,  6:42,  1 user,  load average: 0.75, 0.54, 0.49 fe2bc70918373b10ecd427b1dc6fee5d52f5fbca

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -3,13 +3,12 @@
 make_EHelper(add) {
   
   rtl_add(&t2, &id_dest->val, &id_src->val);
-  rtl_sltu(&t3, &id_dest->val, &t2);
+  rtl_sltu(&t3,&t2,&id_dest->val);
   operand_write(id_dest,&t2);
 
   rtl_update_ZFSF(&t2, id_dest->width);
 
-  //与sub不同，结果值<原始值 sltu无符号
-  rtl_sltu(&t0, &t2, &id_dest->val);
+  rtl_sltu(&t0,&t2,&id_dest->val);
   rtl_or(&t0, &t3, &t0);
   rtl_set_CF(&t0);
 
@@ -23,29 +22,7 @@ make_EHelper(add) {
   print_asm_template2(add);
 }
 
-make_EHelper(sub) {
-  
-  //需要符号扩展的情况decode已经做了 SI2G
-  rtl_sub(&t2, &id_dest->val, &id_src->val);
- 
-  rtl_sltu(&t3, &id_dest->val, &t2);
-  operand_write(id_dest,&t2);
-  rtl_update_ZFSF(&t2, id_dest->width);
 
-  rtl_sltu(&t0, &id_dest->val, &t2);
-  rtl_or(&t0, &t3, &t0);
-  rtl_set_CF(&t0);
-
- //a-(-b)  OF
-  rtl_xor(&t0, &id_dest->val, &id_src->val);
-  rtl_xor(&t1, &id_dest->val, &t2);
-  rtl_and(&t0, &t0, &t1);
-  rtl_msb(&t0, &t0, id_dest->width);
-  rtl_set_OF(&t0);
-
-
-  print_asm_template2(sub);
-}
 
 make_EHelper(cmp) {
   
