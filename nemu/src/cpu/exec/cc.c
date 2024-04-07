cef14b6e3f2ed46e6a1a852a9003b014d9eef801
tracer-ics2017 <tracer@njuics.org>
2024-04-07 20:04:00 +0800

    >  compile 2113665 prayer Linux prayer-virtual-machine 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 athlon i686 GNU/Linux  20:04:00 up 1 day,  5:47,  1 user,  load average: 0.53, 0.44, 0.38 a2c932be6d03da6db9b75c529967317d32a01516

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -13,21 +13,34 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
-    case CC_O:
-      //overflow
-      *dest=cpu.OF==1?1:0;
-    case CC_B:
-      *dest=cpu.CF==1?1:0;
-    case CC_E:
-      *dest=cpu.ZF==1?1:0;
-    case CC_BE:
-      *dest=cpu.ZF||cpu.CF;
-    case CC_S:
-      *dest=cpu.SF==1?1:0;
-    case CC_L:
-      *dest=(cpu.SF!=cpu.OF?1:0);
-    case CC_LE:
-      *dest=(cpu.ZF||cpu.SF!=cpu.OF);
+    case CC_O:{
+      *dest = cpu.OF;
+      break;
+    }
+    case CC_B:{
+      *dest = cpu.CF;
+      break;
+    }
+    case CC_E:{
+      *dest = cpu.ZF;
+      break;
+    }
+    case CC_BE:{
+      *dest = cpu.ZF || cpu.CF;
+      break;
+    }
+    case CC_S:{
+      *dest = cpu.SF;
+      break;
+    }
+    case CC_L:{
+      *dest = cpu.SF != cpu.OF;
+      break;
+    }
+    case CC_LE:{
+      *dest = (cpu.ZF || (cpu.SF != cpu.OF));
+      break;
+    }
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
