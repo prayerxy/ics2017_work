20876d7d672c3ed03cc029d528f874148fffb3b3
tracer-ics2017 <tracer@njuics.org>
2024-04-17 21:18:19 +0800

    >  compile 2113665 prayer Linux prayer-virtual-machine 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 athlon i686 GNU/Linux  21:18:19 up  6:06,  1 user,  load average: 0.23, 0.37, 0.28 11e616487fa2c5a3f0bd3533e64531d1608a34c4

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,8 +1,12 @@
 #include "common.h"
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
+extern size_t get_ramdisk_size();
+extern void ramdisk_read(void *, off_t, size_t);
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  //将ramdisk中从0开始的所有内容放置在default_entry处的位置
+  size_t size = get_ramdisk_size();
+  ramdisk_read(DEFAULT_ENTRY, 0, size); 
   return (uintptr_t)DEFAULT_ENTRY;
 }
--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -15,12 +15,12 @@ int main() {
 #ifdef HAS_PTE
   init_mm();
 #endif
-
+  //注意这里的log()宏不是NEMU中的，nemu与nanos-lite是独立的
   Log("'Hello World!' from Nanos-lite");
   Log("Build time: %s, %s", __TIME__, __DATE__);
-
+  //初始化ramdisk 将一段内存作为磁盘来使用
   init_ramdisk();
-
+  //初始化设备
   init_device();
 
 #ifdef HAS_ASYE
@@ -29,8 +29,9 @@ int main() {
 #endif
 
   init_fs();
-
+  //加载用户程序  返回程序的入口地址
   uint32_t entry = loader(NULL, NULL);
+  //跳转到用户程序入口执行
   ((void (*)(void))entry)();
 
   panic("Should not reach here");
