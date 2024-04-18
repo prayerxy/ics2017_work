ac830ac2ff7590f32822e3869a433294d8bde016
prayerxy <1401662806@qq.com>
2024-04-18 23:35:48 +0800

    修改完善do_syscall，增加sys_none、sys_exit等

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,16 +2,24 @@
 #include "syscall.h"
 
 static inline void do_sys_none(_RegSet*r){
-  SYSCALL_ARG1(r)=1;
+  SYSCALL_ARG1(r)=1;//返回值置1
+}
+static inline void do_sys_exit(_RegSet*r){
+  _halt(SYSCALL_ARG2(r));
 }
 
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
+  //系统调用号在eax寄存器中
 
   switch (a[0]) {
     case SYS_none:
       do_sys_none(r);
+      break;
+    case SYS_exit:
+      //sys_exit中并把第一个参数(退出状态)写到ebx寄存器中。 
+      do_sys_exit(r);
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
