b574bd7847997b7487f6c7b4f4d6265a14d9e1c5
tracer-ics2017 <tracer@njuics.org>
2024-04-23 19:40:38 +0800

    >  run 2113665 prayer Linux prayer-virtual-machine 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 athlon i686 GNU/Linux  19:40:38 up 15:15,  1 user,  load average: 0.66, 0.61, 0.42 2d7dd88d916b6f4b6e1787046510f1b0e52e3e08

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -8,11 +8,26 @@ static inline void do_sys_exit(_RegSet*r){
   _halt(SYSCALL_ARG2(r));
 }
 
+static inline void do_sys_write(_RegSet*r){
+  int fd = SYSCALL_ARG2(r);
+  char* buf = (char*) SYSCALL_ARG3(r);
+  int len = SYSCALL_ARG4(r);
+  if(fd == 1 || fd == 2){
+    for(int i = 0; i < len; i++) {
+      _putc(buf[i]);
+    }
+    //设置返回值
+    SYSCALL_ARG1(r)=len;
+	}
+  Log("fd is not 1 or 2");
+}
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
+  a[1] = SYSCALL_ARG2(r);
+  a[2] = SYSCALL_ARG3(r);
+  a[3] = SYSCALL_ARG4(r);
   //系统调用号在eax寄存器中
-
   switch (a[0]) {
     case SYS_none:
       do_sys_none(r);
@@ -20,6 +35,9 @@ _RegSet* do_syscall(_RegSet *r) {
     case SYS_exit:
       //sys_exit中把第一个参数(退出状态)写到ebx寄存器中。 
       do_sys_exit(r);
+      break;
+    case SYS_write:
+      do_sys_write(r);
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
