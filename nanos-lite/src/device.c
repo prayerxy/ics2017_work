2e3ddf579cf556b63df512ede65f736dbe2fd6fe
tracer-ics2017 <tracer@njuics.org>
2024-05-12 20:01:33 +0800

    >  run 2113665 prayer Linux prayer-virtual-machine 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 athlon i686 GNU/Linux  20:01:33 up 1 day,  4:23,  1 user,  load average: 0.39, 0.28, 0.20 9bee0e9da1a5e15c2736e795b188eb2239a55cdf

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -22,11 +22,37 @@ void dispinfo_read(void *buf, off_t offset, size_t len) {
 void fb_write(const void *buf, off_t offset, size_t len) {
   //用于把buf中的len字节写到屏幕上offset处
   //需要先从offset计算出屏幕上的坐标, 然后调用IOE的_draw_rect()接口.
-  if(len<=0){return;}
-  offset=offset/4;
-  int x=offset%_screen.width;
-  int y=offset/_screen.width;
-  _draw_rect((uint32_t*)buf,x,y,len/4,1);
+  // if(len<=0){return;}
+  // offset=offset/4;
+  // int x=offset%_screen.width;
+  // int y=offset/_screen.width;
+  // _draw_rect((uint32_t*)buf,x,y,len/4,1);
+  assert(offset % 4 == 0 && len % 4 == 0);
+int index, screen_x1, screen_y1, screen_y2;
+  int width=_screen.width;
+  index=offset/4;
+  screen_y1=index/width;
+  screen_x1=index%width;
+  index=(offset+len)/4;
+  screen_y2=index/width;
+  assert(screen_y2>=screen_y1);
+  if(screen_y2==screen_y1)
+  {
+  _draw_rect(buf,screen_x1,screen_y1,len/4,1);
+  return ;
+  }
+  int tempw=width-screen_x1;
+  if(screen_y2-screen_y1==1)
+  {
+  _draw_rect(buf,screen_x1,screen_y1,tempw,1);
+  _draw_rect(buf+tempw * 4 ,0,screen_y2,len/4-tempw,1);
+  return ;
+  }
+  _draw_rect(buf, screen_x1, screen_y1, tempw, 1);
+  int tempy = screen_y2 - screen_y1 - 1;
+  _draw_rect(buf + tempw * 4, 0, screen_y1 + 1, width, tempy);
+  _draw_rect(buf+tempw*4+tempy*width*4,0,screen_y2, len / 4 - tempw - tempy * width,
+  1);
 }
 
 void init_device() {
