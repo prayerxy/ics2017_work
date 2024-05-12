968962431392665883d6cb7c04038afb09eb2280
tracer-ics2017 <tracer@njuics.org>
2024-05-12 19:53:39 +0800

    >  run 2113665 prayer Linux prayer-virtual-machine 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 athlon i686 GNU/Linux  19:53:39 up 1 day,  4:15,  1 user,  load average: 0.28, 0.17, 0.16 253e4a33d53d64b5cf8649d8f8d945a7d1d91f99

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,9 +15,18 @@ size_t events_read(void *buf, size_t len) {
 static char dispinfo[128] __attribute__((used));
 
 void dispinfo_read(void *buf, off_t offset, size_t len) {
+  //用于把字符串dispinfo中offset开始的len字节写到buf中.
+  memcpy(buf, dispinfo + offset, len);
 }
 
 void fb_write(const void *buf, off_t offset, size_t len) {
+  //用于把buf中的len字节写到屏幕上offset处
+  //需要先从offset计算出屏幕上的坐标, 然后调用IOE的_draw_rect()接口.
+  if(len<=0){return;}
+  offset=offset/4;
+  int x=offset%_screen.width;
+  int y=offset/_screen.width;
+  _draw_rect((uint32_t*)buf,x,y,len/4,1);
 }
 
 void init_device() {
@@ -25,4 +34,5 @@ void init_device() {
 
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
+  sprintf(dispinfo, "WIDTH:%d\n HEIGHT:%d\n",_screen.width,_screen.height);
 }
