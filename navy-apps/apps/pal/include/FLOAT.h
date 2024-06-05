cf47d064fc3c2d909cdf6dacf614f67badfdf82b
tracer-ics2017 <tracer@njuics.org>
2024-06-05 15:01:15 +0800

    >  compile 2113665 prayer Linux prayer-virtual-machine 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 athlon i686 GNU/Linux  15:01:15 up  5:41,  1 user,  load average: 1.02, 0.77, 0.55 2205ccb1cfbeab6752d5af01abe572babc808b2

--- navy-apps/apps/pal/include/FLOAT.h
+++ navy-apps/apps/pal/include/FLOAT.h
@@ -4,7 +4,12 @@
 #include "assert.h"
 
 typedef int FLOAT;
-
+FLOAT f2F(float);
+FLOAT F_mul_F(FLOAT, FLOAT);
+FLOAT F_div_F(FLOAT, FLOAT);
+FLOAT Fabs(FLOAT);
+FLOAT Fsqrt(FLOAT);
+FLOAT Fpow(FLOAT, FLOAT);
 static inline int F2int(FLOAT a) {
   if((a&0x80000000)==0){
     return a>>16;
@@ -32,11 +37,6 @@ static inline FLOAT F_div_int(FLOAT a, int b) {
   return F_div_F(a,int2F(b));
 }
 
-FLOAT f2F(float);
-FLOAT F_mul_F(FLOAT, FLOAT);
-FLOAT F_div_F(FLOAT, FLOAT);
-FLOAT Fabs(FLOAT);
-FLOAT Fsqrt(FLOAT);
-FLOAT Fpow(FLOAT, FLOAT);
+
 
 #endif
